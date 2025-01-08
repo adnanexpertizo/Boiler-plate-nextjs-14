78b5f211ab4b784b3c91c3c7f7d9cbfdbdda8c7c
Adnan <adnan.expertizo@gmail.com>
2025-01-08 18:21:56 +0500

    fix: the scr folder

---
+++ src/app/(content)/practice/page.tsx
@@ -0,0 +1,15 @@
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Aurtel Consulting Group | Practice",
+};
+const page = () => {
+  return (
+    <div>
+    PracticeView
+    </div>
+  )
+}
+
+export default page
